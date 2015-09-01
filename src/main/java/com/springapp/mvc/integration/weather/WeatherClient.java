f0ad29b2333bbe8b76e7bbc87bc41a5b94a054c6
Ivan Melnikov <ivan9105@bk.ru>
2015-09-01 23:30:57 +0400

    add base configuration

---
+++ src/main/java/com/springapp/mvc/integration/weather/WeatherClient.java
@@ -0,0 +1,10 @@
+package com.springapp.mvc.integration.weather;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+/**
+ * Created by Иван on 01.09.2015.
+ */
+public class WeatherClient extends WebServiceGatewaySupport{
+
+}
---
+++ src/main/java/com/springapp/mvc/integration/weather/WeatherConfiguration.java
@@ -0,0 +1,28 @@
+package com.springapp.mvc.integration.weather;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+/**
+ * Created by Иван on 01.09.2015.
+ */
+@Configuration
+public class WeatherConfiguration {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.springapp.mvc.integration.weather.schema");
+        return marshaller;
+    }
+
+    @Bean
+    public WeatherClient weatherClient(Jaxb2Marshaller marshaller) {
+        WeatherClient client = new WeatherClient();
+        client.setDefaultUri("http://wsf.cdyne.com/WeatherWS/Weather.asmx");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+}
