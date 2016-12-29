5339d13522df6915a33321d6d6c475838138528e
ivan9105 <ivan9105@bk.ru>
2016-12-30 00:10:24 +0400

    login field part 1

--- src/main/java/com/springapp/mvc/shop/ShopUI.java
+++ src/main/java/com/springapp/mvc/shop/ShopUI.java
@@ -8,31 +8,76 @@ import com.springapp.mvc.shop.user.UserLayout;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.MenuBar;
-import com.vaadin.ui.TabSheet;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.Reindeer;
 
 @Theme("valo")
 @SpringUI(path = "main")
 @SuppressWarnings("serial")
 public class ShopUI extends UI {
+    private static final String TRUSTED_PASSWORD = "admin";
+    private static final String TRUSTED_LOGIN = "admin";
+
     private SpringContextHelper contextHelper;
+    private VerticalLayout screensLayout;
+    private VerticalLayout loginLayout;
 
     @Override
     protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+        setSizeFull();
 
         contextHelper = new SpringContextHelper(VaadinServlet.getCurrent().getServletContext());
 
+        initScreensLayout();
+
+        VerticalLayout loginFieldsLayout = new VerticalLayout();
+        loginFieldsLayout.setMargin(true);
+        loginFieldsLayout.setCaption("Please login to access the application. " +
+                "(Default " + TRUSTED_LOGIN + "/" + TRUSTED_PASSWORD);
+        loginFieldsLayout.setSpacing(true);
+        loginFieldsLayout.setMargin(new MarginInfo(true, true, true, false));
+        loginFieldsLayout.setSizeUndefined();
+
+        TextField loginField = new TextField("User:");
+        loginField.setWidth("300px");
+        loginField.setRequired(true);
+        loginField.setInputPrompt("Input login value");
+        loginField.setInvalidAllowed(false);
+
+        PasswordField passwordField = new PasswordField("Password:");
+        passwordField.setWidth("300px");
+        passwordField.setRequired(true);
+        passwordField.setValue("");
+        passwordField.setNullRepresentation("");
+
+        Button loginButton = new Button("Login", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                //Todo check login + trusted logic
+                setContent(screensLayout);
+            }
+        });
+
+        loginFieldsLayout.addComponents(loginField, passwordField, loginButton);
+
+        loginLayout = new VerticalLayout(loginFieldsLayout);
+        loginLayout.setSizeFull();
+        loginLayout.setComponentAlignment(loginFieldsLayout, Alignment.MIDDLE_CENTER);
+        loginLayout.setStyleName(Reindeer.LAYOUT_BLUE);
+        setContent(loginLayout);
+    }
+
+    private void initScreensLayout() {
+        screensLayout = new VerticalLayout();
+        screensLayout.setMargin(true);
+
         MenuBar menuBar = new MenuBar();
-        layout.addComponent(menuBar);
+        screensLayout.addComponent(menuBar);
 
         final TabSheet tabSheet = new TabSheet();
-        layout.addComponent(tabSheet);
+        screensLayout.addComponent(tabSheet);
 
         MenuBar.MenuItem items = menuBar.addItem("Item management", null);
         items.addItem("Items", new ShopCommand("Items", tabSheet) {
