23b46207f4e14dedb001f0561533c9583adf6048
ivan9105 <ivan9105@bk.ru>
2017-01-06 23:57:55 +0400

    add callback function

--- src/main/webapp/resources/shop.js
+++ src/main/webapp/resources/shop.js
@@ -12,6 +12,15 @@ CategoryClass.fromJson = function (json) {
     return new CategoryClass(obj.id, obj.name, obj.description, obj.level, obj.parentId, obj.child_);
 }
 
+CategoryClass.fromObject = function (obj) {
+    return new CategoryClass(obj.id, obj.name, obj.description, obj.level, obj.parentId, obj.child_);
+}
+
+CategoryClass.toString = function () {
+    return "Id: " + this.id + ",\nName: " + this.name + ",\nDescription: " + this.description +
+        ",\nLevel: " + this.level + ",\nParentId: " + this.parentId;
+}
+
 var ItemClass = function (id, name, description, count, price, categoryId) {
     this.id = id;
     this.name = name;
@@ -26,20 +35,36 @@ ItemClass.fromJson = function (json) {
     return new ItemClass(obj.id, obj.name, obj.description, obj.count, obj.price, obj.categoryId)
 }
 
-function getCategories(host) {
+ItemClass.fromObject = function (obj) {
+    return new ItemClass(obj.id, obj.name, obj.description, obj.count, obj.price, obj.categoryId)
+}
+
+ItemClass.toString = function () {
+    return "Id: " + this.id + ",\nName: " + this.name + ",\nDescription: " + this.description +
+        ",\nCount: " + this.count + ",\nPrice: " + this.price + ",\nCategoryId: " + this.categoryId;
+}
+
+function getCategories(host, callback) {
     var xmlHttpRequest = new XMLHttpRequest();
     xmlHttpRequest.open("GET", host + "/api/shop/categories", true);
-    xmlHttpRequest.onload = function() {
+    xmlHttpRequest.onload = function () {
         var result = [];
         var json = xmlHttpRequest.responseText;
         var obj = JSON.parse(json);
         var length = obj['data'].length;
         for (i = 0; i < length; i++) {
-
+            result.push(CategoryClass.fromObject(obj['data'][i]));
         }
-        //Todo create function callback when init menu, send CategoryClass array use array.push
+        callback(result);
     }
     xmlHttpRequest.send(null);
 }
-//getCategories("http://localhost:8080");
+
+var initMenu = function initTreeMenu(categories) {
+    for (i = 0; i < categories.length; i++) {
+        console.log(categories[i]);
+    }
+}
+
+//getCategories("http://localhost:8080", initMenu);
 
